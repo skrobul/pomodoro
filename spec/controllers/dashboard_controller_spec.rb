7b6fef672ff6cd11a358765641a40ef7ce16a86b
Graham Noel <grahamnoel@gmail.com>
2018-10-27 20:22:36 +0100

    fix broken spec after introducing Devise

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http redirect' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
