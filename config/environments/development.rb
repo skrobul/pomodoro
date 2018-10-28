08ccaddd83f551c46c7a33c401ee971b8a52d02c
Graham Noel <grahamnoel@gmail.com>
2018-10-28 16:00:03 +0000

    expose Devise password reset mailer preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Set default mailer for Devise
+  config.action_mailer.default_url_options = { host: '192.168.57.10', port: 3000 }
 end
