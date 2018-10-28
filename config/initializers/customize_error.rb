36a4543311a0b5da388afb55aaad6e4374fa1773
Graham Noel <grahamnoel@gmail.com>
2018-10-28 17:32:49 +0000

    rubocop fixes

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Don't wrap field errors in a <div> to allow Bootstrap style to remain intact
-# TODO add class to style the 'error field' with a red border
-ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+# TODO add class to style the 'error field' with a red border
+ActionView::Base.field_error_proc = proc { |html_tag, _instance| html_tag }
