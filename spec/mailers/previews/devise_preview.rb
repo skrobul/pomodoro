46580ff48f9e0044527461348cff87478de88e73
Graham Noel <grahamnoel@gmail.com>
2018-10-28 16:24:41 +0000

    devise: style password reset form

--- spec/mailers/previews/devise_preview.rb
+++ spec/mailers/previews/devise_preview.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://192.168.57.10:3000/rails/mailers/devise
 class DevisePreview < ActionMailer::Preview
-
   # Preview this email at http://192.168.57.10:3000/rails/mailers/devise/password_reset
   def password_reset
     Devise::Mailer.reset_password_instructions(User.first, 'token')
   end
-
 end
